30b973ede80049e202f8165059452fc0d761d0ca
Kishan Raja <kishan.raja96@gmail.com>
2022-01-29 12:57:21 -0500

    dropdown menu on homepage

--- home.js
+++ home.js
@@ -1,1 +1,11 @@
+const toggleItems = () => {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
+
+
+$(() => {
+
+$('.dropbtn').on('click', toggleItems)
+
+})
--- projects.js
+++ projects.js
@@ -6,23 +6,36 @@ $(() => {
   currentImgIndex = 0;
   numOfImages = $('.carousel-images').children().length - 1
 
+
+// event listener for clicks on the next button
   $('.next').on('click', () => {
+    //hide current image being shown
      $('.carousel-images').children().eq(currentImgIndex).css('display','none')
+     // if the index of the current image is less than total number of images
      if(currentImgIndex < numOfImages) {
+       //add 1 to the index, showing next image
        currentImgIndex++
      } else {
+       // if our number is outside of available indexes, start at index 0
        currentImgIndex = 0
      }
+    // show image of relevant index
     $('.carousel-images').children().eq(currentImgIndex).css('display','block')
   })
 
+
+//event listener for clicks on the previous button
   $('.previous').on('click', () => {
+    //remove currently displayed image
     $('.carousel-images').children().eq(currentImgIndex).css('display', 'none')
     if(currentImgIndex > 0) {
+   //subtract 1 from the index, showing previous image
       currentImgIndex--
     } else {
+    // if our number is outside of available indexes, start at index 0
       currentImgIndex = numOfImages
     }
+    //show image of relevant index
     $('.carousel-images').children().eq(currentImgIndex).css('display','block')
   })
 
